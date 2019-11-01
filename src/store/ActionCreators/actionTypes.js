710666e69071794cc2a2279be85400a001f479d1
chantelle <maudlin.nk@gmail.com>
2019-11-02 09:12:20 +1100

    Update actionTypes.js

--- src/store/ActionCreators/actionTypes.js
+++ src/store/ActionCreators/actionTypes.js
@@ -1,7 +1,6 @@
 export const SAVE_AIRPORT_TO_STORE = 'SAVE_AIRPORT_TO_STORE';
 export const LOADING_START = 'LOADING_START';
 export const LOADING_END = 'LOADING_END';
-export const LOADING_ERROR = 'LOADING_ERROR';
 export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 export const SEND_AIRPORT_DETAIl = 'SEND_AIRPORT_DETAIl';
