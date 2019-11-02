c3a3cbf7794d1def42be9975f3eb6ec46d5eb939
chantelle <maudlin.nk@gmail.com>
2019-11-02 19:11:13 +1100

    clean up code, remove unessary route component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom"
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { combineReducers } from 'redux'
@@ -18,8 +17,6 @@ import HomePage from "./component/HomePage/HomePage";
 
 
 const middleware = [thunk];
-
-
 let reducer = combineReducers({
     isLoading,
     airports,
@@ -37,13 +34,9 @@ class App extends Component {
     render() {
         return (
             <Provider store={ store }>
-                <BrowserRouter>
                     <Container>
-                        <Switch>
-                            <Route exact path='/' component={ HomePage }/>
-                        </Switch>
+                        <HomePage/>
                     </Container>
-                </BrowserRouter>
             </Provider>
         );
     }
