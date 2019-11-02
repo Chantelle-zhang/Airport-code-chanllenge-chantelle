2a7daa1f6e58774344eabd6d05cde608c654669e
chantelle <maudlin.nk@gmail.com>
2019-11-02 23:07:36 +1100

    add serviceWorker

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 
-registerServiceWorker();
+
 ReactDOM.render(<App/>, document.getElementById('root'));
 
+serviceWorker.register();
