3d282f85294699d36fd64283387e5898f2c3e70f
chantelle <maudlin.nk@gmail.com>
2019-11-02 19:10:43 +1100

    Create App.test.js

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import React  from 'react';
+import { shallow } from "enzyme/build";
+import App from './App'
+import HomePage from "./component/HomePage/HomePage";
+
+
+describe('Check App component', () => {
+
+    it('has one HomePage component ', () => {
+        const wrapper = shallow(<App/>);
+        const homepage = wrapper.find(HomePage);
+        expect(homepage.length).toBe(1);
+
+    });
+})
