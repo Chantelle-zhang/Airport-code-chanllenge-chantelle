33474d648e941b2b76f32d3f4ca6fae79510be6a
chantelle <maudlin.nk@gmail.com>
2019-11-03 17:40:44 +1100

    add localstorage for api data

--- src/store/ActionCreators/actions.js
+++ src/store/ActionCreators/actions.js
@@ -13,13 +13,26 @@ export const fetchData = () => async (dispatch) => {
     });
 
     try {
-        const url = 'https://api.qantas.com/flight/refData/airport';
-        const res = await axios.get(url);
+        const key = 'airports'
+        if (localStorage[key]) {
+            const airports=JSON.parse(localStorage.getItem(key) );
+            console.log('localstorage')
+            dispatch({
+                type: SAVE_AIRPORT_TO_STORE,
+                data: airports
+            });
 
-        dispatch({
-            type: SAVE_AIRPORT_TO_STORE,
-            data: res.data
-        });
+        }
+        else {
+            const url = 'https://api.qantas.com/flight/refData/airport';
+            const res = await axios.get(url);
+            localStorage.setItem(key, JSON.stringify(res.data));
+
+            dispatch({
+                type: SAVE_AIRPORT_TO_STORE,
+                data: res.data
+            });
+        }
 
     } catch ( err ) {
         console.log(err)
