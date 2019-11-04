7e2d549816b8fc3ae06827192216f4ea61591f1d
chantelle <maudlin.nk@gmail.com>
2019-11-04 13:59:35 +1100

    Update Detail.js

--- src/component/Detail/Detail.js
+++ src/component/Detail/Detail.js
@@ -7,11 +7,11 @@ export const Detail = ({ airport }) => {
     return (
         <div>
             <h2 className='text-center'>{ airport.airportName }</h2>
-            <p data-test='airportCode'>Airport Code:{ airport.airportCode }</p>
+            <p data-test='airportCode'>Airport Code: { airport.airportCode }</p>
             <p data-test='city'>City: { airport.city.cityName }</p>
             <p data-test='latitude'>Latitude: { airport.location.latitude }</p>
             <p data-test='longitude'>Longitude: { airport.location.longitude }</p>
-            <p data-test='timeZone'>Time Zone:{ airport.city.timeZoneName }</p>
+            <p data-test='timeZone'>Time Zone: { airport.city.timeZoneName }</p>
         </div>
     )
 };
